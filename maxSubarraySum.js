f466bb7716905ff614108b071a12c8c02300fda6
Aji <caksaaji@gmail.com>
2024-09-25 04:12:42 +0700

    [UPDATE] maxSubarraySum.js : solve

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -1,14 +1,35 @@
-// Write a function called maxSubarraySum which accepts an array of integers and a number called n.
-// The function should calculate the maximum sum of n consecutive elements in the array.
+function maxSubarraySum(arr, n) {
+  // Initialize result value
+  let result = null;
 
-// maxSubarraySum([1,2,5,2,8,1,5],2) // 10
-// maxSubarraySum([1,2,5,2,8,1,5],4) // 17
-// maxSubarraySum([4,2,1,6],1) // 6
-// maxSubarraySum([4,2,1,6,2],4) // 13
-// maxSubarraySum([],4) // null
+  // Check length of arr and value of n matching
+  if (arr.length > 0 && n > 0 && arr.length >= n) {
+    let highestSum = 0;
+    // Loop n times (how many possibilities)
+    for (let i = 0; i < arr.length - (n - 1); i++) {
+      let currentSum = 0;
+      // Summarize each possibilities
+      for (let j = 0; j < n; j++) {
+        currentSum += arr[i + j];
+        // Replace highest summary if higher summary found
+        highestSum = Math.max(highestSum, currentSum);
+      }
+    }
+    result = highestSum;
+  }
 
-
-function maxSubarraySum() {
+  return result;
 }
 
-module.exports = maxSubarraySum;
+// module.exports = maxSubarraySum;
+// Created a new array with 2 string as value based on examples below to make your own checking
+const example = [
+  [[1, 2, 5, 2, 8, 1, 5], 2],
+  [[1, 2, 5, 2, 8, 1, 5], 4],
+  [[4, 2, 1, 6], 1],
+  [[4, 2, 1, 6, 2], 4],
+  [[], 4]
+];
+for (let i = 0; i < example.length; i++) {
+  console.log(`[${example[i][0]}], ${example[i][1]} = ${maxSubarraySum(example[i][0], example[i][1])}`);
+}
